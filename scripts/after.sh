5640fa49f671a752152b25030c67756dc11f8622
Kalyan Kalapala <kalyankalapala@Kalyans-Air-2.lan>
2022-04-19 10:38:52 -0500

    changes

--- scripts/after.sh
+++ scripts/after.sh
@@ -7,10 +7,10 @@ sudo systemctl enable docker
 sudo systemctl restart docker
 
 ### install docker-compose ####
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-sudo chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+#sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+#sudo chmod +x /usr/local/bin/docker-compose
+#ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#docker-compose --version
 
 #### docker-compose up ####
 cd /home/ec2-user/dockernodejs
